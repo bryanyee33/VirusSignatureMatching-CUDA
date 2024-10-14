3ca1fc113e69fedc1634000097067bd5fdcc0c7d
Bryan <bryanyee33@gmail.com>
2024-10-14 20:12:45 +0800

    Add phred score calculation

--- kernel_skeleton.cu
+++ kernel_skeleton.cu
@@ -4,15 +4,8 @@
 #define SAMPLE_MAX_LEN 200000
 #define SIGNATURE_MAX_LEN 10000
 
-void check_cuda_errors() {
-    cudaError_t rc = cudaGetLastError();
-    if (rc != cudaSuccess) {
-        printf("Last CUDA error %s\n", cudaGetErrorString(rc));
-    }
-}
-
 __global__ void getMatches(const char* __restrict d_sample_seq, const char* __restrict d_sample_qual,
-        const char* __restrict d_signature_seq, const char* __restrict d_signature_qual) {
+        const char* __restrict d_signature_seq) {
 
     const char *sample = &d_sample_seq[SAMPLE_MAX_LEN * blockIdx.x];
     const char *signature = &d_signature_seq[SIGNATURE_MAX_LEN * threadIdx.x];
@@ -20,9 +13,13 @@ __global__ void getMatches(const char* __restrict d_sample_seq, const char* __re
     for (int i = 0; i < SAMPLE_MAX_LEN; ++i) {
         for (int j = 0; j < SIGNATURE_MAX_LEN; ++j) {
             if (!signature[j]) { // Signature ended; found match
-                //i to j-1
+                const char *qual = &d_sample_qual[SAMPLE_MAX_LEN * blockIdx.x + i];
+                int tot = 0;
+                for (int s = 0; s < j; ++s) {
+                    tot += qual[s] - 33;
+                }
                 
-                printf("Sample: %d, Sig: %d, %c\n", blockIdx.x, threadIdx.x, signature[j - 1]);
+                printf("Sample: %d, Sig: %d, Avg: %f\n", blockIdx.x, threadIdx.x, (float)tot/j);
                 break;
             } else if (!sample[i + j] || sample[i + j] != 'N' && signature[j] != 'N' &&
                     sample[i + j] != signature[j]) { // No match
@@ -57,18 +54,16 @@ void runMatcher(const std::vector<klibpp::KSeq>& samples, const std::vector<klib
     char *h_sample_seq = (char*)calloc(sample_arr_len, sizeof(char));
     char *h_sample_qual = (char*)calloc(sample_arr_len, sizeof(char));
     char *h_signature_seq = (char*)calloc(signature_arr_len, sizeof(char));
-    char *h_signature_qual = (char*)calloc(signature_arr_len, sizeof(char));
 
     for (int i = 0; i < samples.size(); ++i) {
         memcpy(&h_sample_seq[i * SAMPLE_MAX_LEN], &samples[i].seq[0], samples[i].seq.size());
-        memcpy(&h_sample_qual[i * SAMPLE_MAX_LEN], &samples[i].seq[0], samples[i].qual.size());
+        memcpy(&h_sample_qual[i * SAMPLE_MAX_LEN], &samples[i].qual[0], samples[i].qual.size());
     }
     for (int i = 0; i < signatures.size(); ++i) {
         memcpy(&h_signature_seq[i * SIGNATURE_MAX_LEN], &signatures[i].seq[0], signatures[i].seq.size());
-        memcpy(&h_signature_qual[i * SIGNATURE_MAX_LEN], &signatures[i].seq[0], signatures[i].qual.size());
     }
     
-    char *d_sample_seq, *d_sample_qual, *d_signature_seq, *d_signature_qual;
+    char *d_sample_seq, *d_sample_qual, *d_signature_seq;
 
     cudaMallocManaged(&d_sample_seq, sample_arr_len);
     cudaMallocManaged(&d_sample_qual, sample_arr_len);
@@ -76,17 +71,9 @@ void runMatcher(const std::vector<klibpp::KSeq>& samples, const std::vector<klib
     cudaMemcpy(d_sample_qual, h_sample_qual, sample_arr_len, cudaMemcpyHostToDevice);
 
     cudaMallocManaged(&d_signature_seq, signature_arr_len);
-    cudaMallocManaged(&d_signature_qual, signature_arr_len);
     cudaMemcpy(d_signature_seq, h_signature_seq, signature_arr_len, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_signature_qual, h_signature_qual, signature_arr_len, cudaMemcpyHostToDevice);
 
-    getMatches<<<samples.size(), signatures.size()>>>(d_sample_seq, d_sample_qual, d_signature_seq, d_signature_qual);
-    check_cuda_errors();
-
-    cudaFree(d_sample_seq);
-    cudaFree(d_sample_qual);
-    cudaFree(d_signature_seq);
-    cudaFree(d_signature_qual);
+    getMatches<<<samples.size(), signatures.size()>>>(d_sample_seq, d_sample_qual, d_signature_seq);
 
     // unsigned long long result = 0.0f;
     // // Run several iterations to get an average measurement
@@ -100,8 +87,10 @@ void runMatcher(const std::vector<klibpp::KSeq>& samples, const std::vector<klib
     // // cudaMemcpyFromSymbol will implicitly synchronize CPU and GPU
     // cudaMemcpyFromSymbol(&result, dResult, sizeof(unsigned long long));
 
+    cudaFree(d_sample_seq);
+    cudaFree(d_sample_qual);
+    cudaFree(d_signature_seq);
     free(h_sample_seq);
     free(h_sample_qual);
     free(h_signature_seq);
-    free(h_signature_qual);
 }
