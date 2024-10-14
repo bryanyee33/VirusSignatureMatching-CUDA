ddc244940a9a6b55522fc80fac898232fc213a21
Bryan <bryanyee33@gmail.com>
2024-10-14 18:30:56 +0800

    Initial version without phred score

---
+++ gpu_job.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+## Change this to a job name you want
+#SBATCH --job-name=assignment2
+
+## Change based on length of job and `sinfo` partitions available
+##SBATCH --partition=gpu
+
+## Request for a specific type of node
+## Commented out for now, change if you need one
+#SBATCH --constraint=xgph
+
+## gpu:1 ==> any gpu. For e.g., --gres=gpu:a100-40:1 gets you one of the A100 GPU shared instances
+#SBATCH --gpus=a100-40
+
+## Must change this based on how long job will take. We are just expecting 30 seconds for now
+#SBATCH --time=00:10:00
+
+## Probably no need to change anything here
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+##SBATCH --cpu_bind=core
+
+## May want to change this depending on how much host memory you need
+#SBATCH --mem-per-cpu=20G
+
+## Just useful logfile names
+#SBATCH --output=a2_%j.slurmlog
+#SBATCH --error=a2_%j.slurmlog
+
+
+echo "Job is running on $(hostname), started at $(date)"
+
+# Set the nvidia compiler directory
+# NVCC=/usr/local/cuda/bin/nvcc
+
+# Check that it exists and print some version info
+# [[ -f $NVCC ]] || { echo "ERROR: NVCC Compiler not found at $NVCC, exiting..."; exit 1; }
+# echo "NVCC info: $($NVCC --version)"
+
+# Actually compile the code
+echo -e "\n====> Compiling...\n"
+# $NVCC -arch native -O3 --std=c++17 -o sum sum.cu
+make
+
+echo -e "\n====> Running...\n"
+./matcher samp.fastq sig.fasta
+# ./bench-a100 samp.fastq sig.fasta
+
+echo -e "\n====> Finished running.\n"
+
+echo -e "\nJob completed at $(date)"
--- kernel_skeleton.cu
+++ kernel_skeleton.cu
@@ -1,6 +1,107 @@
 #include "kseq/kseq.h"
 #include "common.h"
 
-void runMatcher(const std::vector<klibpp::KSeq>& samples, const std::vector<klibpp::KSeq>& signatures, std::vector<MatchResult>& matches) {
+#define SAMPLE_MAX_LEN 200000
+#define SIGNATURE_MAX_LEN 10000
 
+void check_cuda_errors() {
+    cudaError_t rc = cudaGetLastError();
+    if (rc != cudaSuccess) {
+        printf("Last CUDA error %s\n", cudaGetErrorString(rc));
+    }
+}
+
+__global__ void getMatches(const char* __restrict d_sample_seq, const char* __restrict d_sample_qual,
+        const char* __restrict d_signature_seq, const char* __restrict d_signature_qual) {
+
+    const char *sample = &d_sample_seq[SAMPLE_MAX_LEN * blockIdx.x];
+    const char *signature = &d_signature_seq[SIGNATURE_MAX_LEN * threadIdx.x];
+
+    for (int i = 0; i < SAMPLE_MAX_LEN; ++i) {
+        for (int j = 0; j < SIGNATURE_MAX_LEN; ++j) {
+            if (!signature[j]) { // Signature ended; found match
+                //i to j-1
+                
+                printf("Sample: %d, Sig: %d, %c\n", blockIdx.x, threadIdx.x, signature[j - 1]);
+                break;
+            } else if (!sample[i + j] || sample[i + j] != 'N' && signature[j] != 'N' &&
+                    sample[i + j] != signature[j]) { // No match
+                break;
+            }
+        }
+    }
+    // const int id = threadIdx.x + blockIdx.x * blockDim.x;
+
+    // __shared__ unsigned long long data[BLOCK_SIZE];
+
+    // data[threadIdx.x] = input[id];
+    // __syncthreads();
+
+    // for (int i = 1; i < 256; i <<= 1) {
+    //     if (threadIdx.x % (i << 1) == 0) {
+    //         data[threadIdx.x] += data[threadIdx.x + i];
+    //     }
+    //     __syncthreads();
+    // }
+
+    // if (threadIdx.x == 0) {
+    //     atomicAdd(&dResult, data[0]);
+    // }
+}
+
+void runMatcher(const std::vector<klibpp::KSeq>& samples, const std::vector<klibpp::KSeq>& signatures, std::vector<MatchResult>& matches) {
+    int sample_arr_len = samples.size() * SAMPLE_MAX_LEN;
+    int signature_arr_len = signatures.size() * SIGNATURE_MAX_LEN;
+
+    // calloc() to initialise all to '\0'; set to max possible length for each type.
+    char *h_sample_seq = (char*)calloc(sample_arr_len, sizeof(char));
+    char *h_sample_qual = (char*)calloc(sample_arr_len, sizeof(char));
+    char *h_signature_seq = (char*)calloc(signature_arr_len, sizeof(char));
+    char *h_signature_qual = (char*)calloc(signature_arr_len, sizeof(char));
+
+    for (int i = 0; i < samples.size(); ++i) {
+        memcpy(&h_sample_seq[i * SAMPLE_MAX_LEN], &samples[i].seq[0], samples[i].seq.size());
+        memcpy(&h_sample_qual[i * SAMPLE_MAX_LEN], &samples[i].seq[0], samples[i].qual.size());
+    }
+    for (int i = 0; i < signatures.size(); ++i) {
+        memcpy(&h_signature_seq[i * SIGNATURE_MAX_LEN], &signatures[i].seq[0], signatures[i].seq.size());
+        memcpy(&h_signature_qual[i * SIGNATURE_MAX_LEN], &signatures[i].seq[0], signatures[i].qual.size());
+    }
+    
+    char *d_sample_seq, *d_sample_qual, *d_signature_seq, *d_signature_qual;
+
+    cudaMallocManaged(&d_sample_seq, sample_arr_len);
+    cudaMallocManaged(&d_sample_qual, sample_arr_len);
+    cudaMemcpy(d_sample_seq, h_sample_seq, sample_arr_len, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_sample_qual, h_sample_qual, sample_arr_len, cudaMemcpyHostToDevice);
+
+    cudaMallocManaged(&d_signature_seq, signature_arr_len);
+    cudaMallocManaged(&d_signature_qual, signature_arr_len);
+    cudaMemcpy(d_signature_seq, h_signature_seq, signature_arr_len, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_signature_qual, h_signature_qual, signature_arr_len, cudaMemcpyHostToDevice);
+
+    getMatches<<<samples.size(), signatures.size()>>>(d_sample_seq, d_sample_qual, d_signature_seq, d_signature_qual);
+    check_cuda_errors();
+
+    cudaFree(d_sample_seq);
+    cudaFree(d_sample_qual);
+    cudaFree(d_signature_seq);
+    cudaFree(d_signature_qual);
+
+    // unsigned long long result = 0.0f;
+    // // Run several iterations to get an average measurement
+    // for (unsigned long long i = 0; i < TIMING_ITERATIONS; i++)
+    // {
+    //     // Reset acummulated result to 0 in each run
+    //     cudaMemcpyToSymbol(dResult, &result, sizeof(unsigned long long));
+    //     func<<<gridDim, blockDim>>>(dValsPtr, N);
+    // }
+
+    // // cudaMemcpyFromSymbol will implicitly synchronize CPU and GPU
+    // cudaMemcpyFromSymbol(&result, dResult, sizeof(unsigned long long));
+
+    free(h_sample_seq);
+    free(h_sample_qual);
+    free(h_signature_seq);
+    free(h_signature_qual);
 }
