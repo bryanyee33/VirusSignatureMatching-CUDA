96c216233fdb7518d62a82cee43bc34f315baa2e
Bryan <bryanyee33@gmail.com>
2024-10-16 13:34:09 +0800

    Fix non-match when signature is max length

--- gpu_job.sh
+++ gpu_job.sh
@@ -8,12 +8,12 @@
 
 ## Request for a specific type of node
 ## Commented out for now, change if you need one
-##SBATCH --constraint=xgph
-#SBATCH --constraint=xgpi
-
 ## gpu:1 ==> any gpu. For e.g., --gres=gpu:a100-40:1 gets you one of the A100 GPU shared instances
-##SBATCH --gpus=a100-40
-#SBATCH --gpus=h100-96
+
+#SBATCH --constraint=xgph
+#SBATCH --gpus=a100-40
+##SBATCH --gpus=h100-96
+##SBATCH --constraint=xgpi
 
 ## Must change this based on how long job will take. We are just expecting 30 seconds for now
 #SBATCH --time=00:10:00
@@ -46,9 +46,16 @@ echo -e "\n====> Compiling...\n"
 make
 
 echo -e "\n====> Running...\n"
-./matcher samp_100%.fastq sig.fasta
+# nsys profile --stats=true ./matcher samp_100%.fastq sig.fasta
+# ./matcher samp_100%.fastq sig.fasta
 # ./bench-a100 samp_100%.fastq sig.fasta
-./bench-h100 samp_100%.fastq sig.fasta
+# ./bench-h100 samp_100%.fastq sig.fasta
+
+# ./matcher samp_200%.fastq sig_100%.fasta
+# ./bench-a100 samp_200%.fastq sig_100%.fasta
+
+./matcher samp.fastq sig.fasta
+./bench-a100 samp.fastq sig.fasta
 
 echo -e "\n====> Finished running.\n"
 
--- kernel_skeleton_sm.cu
+++ kernel_skeleton_sm.cu
@@ -4,19 +4,51 @@
 #define SAMPLE_MAX_LEN 200000
 #define SIGNATURE_MAX_LEN 10000
 
+// SAMPLE_SMEM_SIZE = SAMPLE_MAX_LEN / 2 + SIGNATURE_MAX_LEN
+#define SAMPLE_SMEM_SIZE 110000
+
+void check_cuda_errors()
+{
+    cudaError_t rc;
+    rc = cudaGetLastError();
+    if (rc != cudaSuccess)
+    {
+        printf("Last CUDA error %s\n", cudaGetErrorString(rc));
+    }
+
+}
+
 __global__ void getMatches(const char* __restrict d_sample_seq, const char* __restrict d_sample_qual, const char* __restrict d_signature_seq,
         double* __restrict match_scores, unsigned short* __restrict match_idx, int* __restrict match_count) {
 
+    // Half of sample so it will always fit in shared memory; A size of SIGNATURE_MAX_LEN needs to be copied twice
+    extern __shared__ char s_sample[];
     const char *sample = &d_sample_seq[SAMPLE_MAX_LEN * blockIdx.x];
     const char *signature = &d_signature_seq[SIGNATURE_MAX_LEN * threadIdx.x];
 
-    for (int i = 0; i < SAMPLE_MAX_LEN; ++i) {
-        if (!sample[i]) { // Sample ended; no match
+    // Copy 1st half + SIGNATURE_MAX_LEN of sample into shared memory
+    int num_per_thread = (SAMPLE_SMEM_SIZE + blockDim.x - 1) / blockDim.x; // Ceiling
+    if (threadIdx.x == blockDim.x - 1) { // Last thread
+        memcpy(&s_sample[threadIdx.x * num_per_thread], &sample[threadIdx.x * num_per_thread],
+               SAMPLE_SMEM_SIZE - num_per_thread * threadIdx.x);
+    } else {
+        memcpy(&s_sample[threadIdx.x * num_per_thread], &sample[threadIdx.x * num_per_thread],
+               num_per_thread);
+    }
+
+    // if (threadIdx.x == 0) {
+    //     memcpy(s_sample, sample, SAMPLE_SMEM_SIZE);
+    // }
+     __syncthreads();
+
+    for (int i = 0; i < (SAMPLE_MAX_LEN >> 1); ++i) {
+        if (!s_sample[i]) { // Sample ended; no match
             return;
         }
 
-        for (int j = 0; j < SIGNATURE_MAX_LEN + 1; ++j) { // +1 iteration for when signature len == SIGNATURE_MAX_LEN
-            if (j == SIGNATURE_MAX_LEN || !signature[j]) { // Signature ended; found match
+        for (int j = 0; j < SIGNATURE_MAX_LEN; ++j) {
+            // TODO: FIX WHEN SIGNATURE == MAX LEN
+            if (!signature[j]) { // Signature ended; found match
                 const char *qual = &d_sample_qual[SAMPLE_MAX_LEN * blockIdx.x + i];
                 int tot = 0;
                 for (int s = 0; s < j; ++s) {
@@ -29,8 +61,48 @@ __global__ void getMatches(const char* __restrict d_sample_seq, const char* __re
                 match_idx[(idx << 1) + 1] = threadIdx.x; // Signature idx
                 return;
 
-            } else if (!sample[i + j] || sample[i + j] != 'N' && signature[j] != 'N' &&
-                    sample[i + j] != signature[j]) { // No match
+            } else if (!s_sample[i + j] || s_sample[i + j] != 'N' && signature[j] != 'N' &&
+                    s_sample[i + j] != signature[j]) { // No match
+                break;
+            }
+        }
+    }
+
+    // Copy 2nd half of sample into shared memory
+    num_per_thread = ((SAMPLE_MAX_LEN >> 1) + blockDim.x - 1) / blockDim.x; // Ceiling
+    if (threadIdx.x == blockDim.x - 1) { // Last thread
+        memcpy(&s_sample[threadIdx.x * num_per_thread], &sample[(SAMPLE_MAX_LEN >> 1) + threadIdx.x * num_per_thread],
+               (SAMPLE_MAX_LEN >> 1) - num_per_thread * threadIdx.x);
+        s_sample[(SAMPLE_MAX_LEN >> 1)] = 0; // Set next char to '\0' to indicate end
+    } else {
+        memcpy(&s_sample[threadIdx.x * num_per_thread], &sample[(SAMPLE_MAX_LEN >> 1) + threadIdx.x * num_per_thread],
+               num_per_thread);
+    }
+
+    __syncthreads();
+
+    for (int i = 0; i < (SAMPLE_MAX_LEN >> 1); ++i) {
+        if (!s_sample[i]) { // Sample ended; no match
+            return;
+        }
+
+        for (int j = 0; j < SIGNATURE_MAX_LEN; ++j) {
+            // TODO: FIX WHEN SIGNATURE == MAX LEN
+            if (!signature[j]) { // Signature ended; found match
+                const char *qual = &d_sample_qual[SAMPLE_MAX_LEN * blockIdx.x + i];
+                int tot = 0;
+                for (int s = 0; s < j; ++s) {
+                    tot += qual[s] - 33;
+                }
+
+                int idx = atomicAdd(match_count, 1); // Each idx will be unique (no need synchronisation)
+                match_scores[idx] = (double)tot / j;
+                match_idx[idx << 1] = blockIdx.x; // Sample idx
+                match_idx[(idx << 1) + 1] = threadIdx.x; // Signature idx
+                return;
+
+            } else if (!s_sample[i + j] || s_sample[i + j] != 'N' && signature[j] != 'N' &&
+                    s_sample[i + j] != signature[j]) { // No match
                 break;
             }
         }
@@ -55,12 +127,12 @@ void runMatcher(const std::vector<klibpp::KSeq>& samples, const std::vector<klib
     unsigned short *match_idx; // [Samp_idx_1, Sig_idx_1, Samp_idx_2, Sig_idx_2, ...]
 
     // Combine each string array into 1 for transfer
-    for (int i = 0; i < signatures.size(); ++i) {
-        memcpy(&h_signature_seq[i * SIGNATURE_MAX_LEN], &signatures[i].seq[0], signatures[i].seq.size());
-    }
     for (int i = 0; i < samples.size(); ++i) {
         memcpy(&h_sample_seq[i * SAMPLE_MAX_LEN], &samples[i].seq[0], samples[i].seq.size());
         memcpy(&h_sample_qual[i * SAMPLE_MAX_LEN], &samples[i].qual[0], samples[i].qual.size());
+    }
+    for (int i = 0; i < signatures.size(); ++i) {
+        memcpy(&h_signature_seq[i * SIGNATURE_MAX_LEN], &signatures[i].seq[0], signatures[i].seq.size());
     }
 
     // Use cudaMallocHost() to write straight to host, since only a small number of matches should be found
@@ -80,8 +152,10 @@ void runMatcher(const std::vector<klibpp::KSeq>& samples, const std::vector<klib
     cudaMemcpy(d_sample_qual, h_sample_qual, sample_arr_len, cudaMemcpyHostToDevice);
     cudaMemcpy(d_signature_seq, h_signature_seq, signature_arr_len, cudaMemcpyHostToDevice);
 
+    cudaFuncSetAttribute(getMatches, cudaFuncAttributeMaxDynamicSharedMemorySize, SAMPLE_SMEM_SIZE);
     // 1 Sample per block; Each thread in block corresponds to 1 signature
-    getMatches<<<samples.size(), signatures.size()>>>(d_sample_seq, d_sample_qual, d_signature_seq, match_scores, match_idx, match_count);
+    getMatches<<<samples.size(), signatures.size(), SAMPLE_SMEM_SIZE>>>(d_sample_seq, d_sample_qual, d_signature_seq, match_scores, match_idx, match_count);
+    check_cuda_errors();
     cudaDeviceSynchronize();
 
     int h_match_count;
